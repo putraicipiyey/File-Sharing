3cef523bfe22baf198555a9cde28009e887ef0d7
putraicipiyey <88121146+putraicipiyey@users.noreply.github.com>
2022-05-18 06:00:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,7 +102,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/demusplace"
         )
 
     async def stop(self, *args):
